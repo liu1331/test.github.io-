6755e0f0fc2594f7e47b9cd1c0cb3819af22fa02
liu1331 <alenn1441@gmail.com>
2021-10-17 13:11:32 +0300

    rotestest

--- src/components/AppRouter/AppRouter.tsx
+++ src/components/AppRouter/AppRouter.tsx
@@ -8,7 +8,7 @@ import { RoutePath, Routes } from '../../routing/routes'
 const AppRouter = () => {
     return (
         <Switch>
-            {Routes.map((route)=><Route path={route.path} component={route.component} exact={route.exact}/>)}
+            {Routes.map((route)=><Route path={route.path+'/index.html'} component={route.component} exact={route.exact}/>)}
             <Redirect to={RoutePath.PRODUCT_CONTENT}/>
         </Switch>
     )
